3f3668e1170b3be09f1d7fe931607a2daaf3ba19
Donald Tong Mac <donaldtong24@gmail.com>
2025-04-14 23:02:33 -0400

    okay working again and added surface normals

--- include/sphere.h
+++ include/sphere.h
@@ -1,15 +1,15 @@
 #ifndef SPHERE_H
 #define SPHERE_H
-
+#include "common.h"
 #include "hittable.h"
-#include "vec3.h"
+//#include "vec3.h"
 
 class sphere : public hittable {
     public:
         sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
 
         bool hit(const ray& r,double ray_tmin,double ray_tmax, hit_record& rec) const override {
-            vec3 oc = r.origin() - center;
+            vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
             auto c = oc.length_squared() - radius*radius;
@@ -29,7 +29,8 @@ class sphere : public hittable {
 
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center)/  radius;
+            rec.set_face_normal(r, outward_normal);
             return true;
         }
 
